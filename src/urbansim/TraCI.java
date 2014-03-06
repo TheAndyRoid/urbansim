342c5d663203fa12ab42e48929e5153bf420bc4b
Andrew Skinner <Andrew.Skinner@TheAndyRoid.com>
2014-03-06 17:46:49 +0000

    Mobile agents are created dynamically by the SUMO simulation and move around the simulation

---
+++ src/urbansim/TraCI.java
@@ -0,0 +1,131 @@
+package urbansim;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map.Entry;
+
+import it.polito.appeal.traci.MultiQuery;
+import it.polito.appeal.traci.StepAdvanceListener;
+import it.polito.appeal.traci.SumoTraciConnection;
+import it.polito.appeal.traci.Vehicle;
+import it.polito.appeal.traci.VehicleLifecycleObserver;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+
+import sim.engine.SimState;
+import sim.engine.Steppable;
+import sim.util.Double2D;
+
+public class TraCI implements Steppable{
+	
+	
+	private SumoTraciConnection conn ;	
+	
+	
+	public TraCI(){	
+		BasicConfigurator.configure();
+		
+		conn = new SumoTraciConnection(
+				"/home/andyroid/uni/cs4526/Application/test/sumo_maps/box1l/test.sumo.cfg",  // config file
+				12345                                  // random seed
+				);
+		try {
+			conn.runServer();
+			
+			System.out.println("Map bounds are: " + conn.queryBounds());
+			
+		/*	for (int i = 0; i < 4; i++) {
+				int time = conn.getCurrentSimStep();
+				Collection<Vehicle> vehicles = conn.getVehicleRepository().getAll().values();
+				
+				System.out.println("At time step " + time + ", there are "
+						+ vehicles.size() + " vehicles: " + vehicles);
+				for(Vehicle v:vehicles){
+					System.out.println("Vehicle ID:" + v.getID());
+					System.out.println("Vehicle Position:" + v.getPosition());
+					
+				}
+				
+				conn.nextSimStep();
+			}*/
+			
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+	
+	public void addVehicleLifecycleObserver(SimState state){
+		conn.addVehicleLifecycleObserver((VehicleLifecycleObserver) state);	
+	}
+	
+	
+	public void step(SimState state) {
+		UrbanSim urbansim = (UrbanSim) state;
+		try {
+			conn.nextSimStep();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//update mobile positions here
+		
+		MultiQuery mq = conn.makeMultiQuery();
+		for(Entry<String, Agent> entry: urbansim.mobileAgents.entrySet()){
+			Agent tmp = entry.getValue();
+			mq.add(tmp.v.queryReadPosition());	
+		}
+		try {
+			mq.run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Multiquery done!");
+		
+		for(Entry<String, Agent> entry: urbansim.mobileAgents.entrySet()){
+			Agent tmp = entry.getValue();
+			
+			Point2D pos = new Point2D.Double();
+			try {
+				 	pos= tmp.v.getPosition();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}				
+			
+			tmp.setPosition(urbansim,
+					new Double2D(
+							pos.getX(),
+							pos.getY()
+							)						
+					)	;
+		}
+		
+		
+	}
+	public void close(){
+		try {
+			conn.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LogManager.shutdown();
+	}
+
+
+
+	
+}
--- src/urbansim/UrbanSim.java
+++ src/urbansim/UrbanSim.java
@@ -1,12 +1,22 @@
 package urbansim;
 
+import it.polito.appeal.traci.Vehicle;
+import it.polito.appeal.traci.VehicleLifecycleObserver;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import sim.engine.*;
 import sim.util.*;
 import sim.field.continuous.*;
 import sim.field.grid.DoubleGrid2D;
 import sim.field.grid.SparseGrid2D;
 
-public class UrbanSim extends SimState {
+public class UrbanSim extends SimState implements VehicleLifecycleObserver {
 
 	private void getBounds() {
 
@@ -16,43 +26,44 @@ public class UrbanSim extends SimState {
 	private int GRID_WIDTH = 100, GRID_HEIGHT = 100;
 
 	// data
-	public Continuous2D agentPos = new Continuous2D(1.0, 10, 10);
+	public Continuous2D agentPos = new Continuous2D(1.0, 500, 500);
 	public int numAgents = 5;
 	public ParallelSequence SAgents;
-
-	public UrbanSim(long seed) {
+	public TraCI traci;
+	
+	//Map SUMO strings to agents
+	public Map<String,Agent> mobileAgents = new HashMap<String,Agent>();
+	public List<Agent> stationaryAgents = new ArrayList<Agent>();
+	
+	public UrbanSim(long seed) {	
 
 		super(seed);
 		agents = new Agent[numAgents];
 	}
-
+	
+	//Array of all agents
 	Agent[] agents;
 
 	// Setup the simulation here
 	public void start() {
 		super.start();
+		schedule.clear();
 		
-		agentPos.clear();
-		//add  agents to the
-		for(int i = 0; i <numAgents; i++){			
-			agents[i] = new Agent(this,	new Double2D(
-					random.nextDouble()*agentPos.getWidth(),
-					 random.nextDouble() * agentPos.getHeight()
-				));
-			System.out.print(Integer.toString(i)+",");	
-			
-			
-			
-			
-			
+		agentPos.clear();	
+		mobileAgents.clear();
+		stationaryAgents.clear();
+		if(traci != null){
+			traci.close();
+			System.out.println("Close");
 		}
-		//create parallel
-		SAgents = new ParallelSequence(agents,1);
 		
 		
-		//Setup the call schedule
-		schedule.scheduleRepeating(SAgents);
 		
+		//Create the motion aware stepper
+		traci = new TraCI();
+		traci.addVehicleLifecycleObserver(this);
+		
+		schedule.scheduleRepeating(Schedule.EPOCH,traci);
 		
 		
 		System.out.println();
@@ -63,5 +74,68 @@ public class UrbanSim extends SimState {
 		System.exit(0);
 
 	}
+	
+	//re-create the schedule
+	private void reshedule(){
+		schedule.clear();
+		
+		
+		Agent[] magent = mobileAgents.values().toArray(new Agent[mobileAgents.size()]); 
+		Agent[] sagent = stationaryAgents.toArray(new Agent[stationaryAgents.size()]);
+		//Create agent array for parallel
+		agents = new Agent[magent.length + sagent.length];
+		
+		System.arraycopy(magent,0,agents,0,magent.length);
+		System.arraycopy(sagent,0,agents,magent.length,sagent.length);
+				
+		
+		//create parallel
+		SAgents = new ParallelSequence(agents,8);
+		
+		
+		//Setup the call schedule for all agents
+		schedule.scheduleRepeating(SAgents);
+		
+		
+		schedule.scheduleRepeating(traci);
+		
+	}
+	
+	
+	
+	@Override
+	//Vehicle was created by sumo
+	public void vehicleDeparted(Vehicle vehicle) {
+		System.out.println("new Vehicle:" + vehicle.getID());
+		//Get position
+		Point2D pos = new Point2D.Double();		
+		//Create new agent
+		Agent tmp = new Agent();		
+		tmp.v = vehicle;
+		//Add agent to mobile agent list
+		mobileAgents.put(vehicle.getID(),tmp);		
+		reshedule();
+		
+	}
+
+	//Vehicle was destroyed by sumo
+	public void vehicleArrived(Vehicle vehicle) {
+		System.out.println("dead Vehicle");
+		Agent tmp = mobileAgents.remove(vehicle.getID());
+		//Remove from graphic
+		agentPos.remove(tmp);
+		reshedule();
+	}
+
+	@Override
+	public void vehicleTeleportStarting(Vehicle vehicle) {
+		// TODO Auto-generated method stub		
+	}
+	@Override
+	public void vehicleTeleportEnding(Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
